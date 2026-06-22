a9bdd378396f0495ef22d480223094557c0d2883
Pongsapak <riddiqulus@gmail.com>
2026-06-22 17:34:42 +0700

    feat: initialize juniorr-blog project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section className="p-8">
+      <h1 className="text-4xl font-bold text-blue-600">
+        My First React App on Vite
+      </h1>
+    </section>
+  );
+}
+
+export default App;
